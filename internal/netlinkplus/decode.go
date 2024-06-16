a16405cae5a625b9c934fe3429acbc46d3f64292
master <tolkoplus@gmail.com>
2024-06-17 00:07:25 +0300

    File splitting netlinkplus

--- internal/netlinkplus/decode.go
+++ internal/netlinkplus/decode.go
@@ -7,59 +7,10 @@ import (
 
 	"github.com/Gazmasater/netlink/internal/data"
 
-	"github.com/Gazmasater/netlink/pkg/logger"
 	"github.com/mdlayher/netlink"
 	"github.com/pkg/errors"
 	"golang.org/x/sys/unix"
 )
-
-type (
-	proto      uint8
-	PacketInfo struct {
-		SrcIP    string
-		DstIP    string
-		SrcPort  uint16
-		DstPort  uint16
-		Protocol proto
-		Flag     uint16
-		logger   logger.TypeOfLogger
-	}
-)
-
-func (p *PacketInfo) SetLogger(log logger.TypeOfLogger) {
-	p.logger = log
-}
-
-func (pkt *PacketInfo) IsReady() bool {
-	requiredFlags := uint16(data.NFTNL_TRACE_NETWORK_HEADER | data.NFTNL_TRACE_TRANSPORT_HEADER)
-	return pkt.Flag&requiredFlags == requiredFlags
-}
-
-func (p proto) String() string {
-	switch p {
-	case unix.IPPROTO_TCP:
-		return "tcp"
-	case unix.IPPROTO_UDP:
-		return "udp"
-	case unix.IPPROTO_UDPLITE:
-		return "udplite"
-	case unix.IPPROTO_ESP:
-		return "esp"
-	case unix.IPPROTO_AH:
-		return "ah"
-	case unix.IPPROTO_ICMP:
-		return "icmp"
-	case unix.IPPROTO_ICMPV6:
-		return "icmpv6"
-	case unix.IPPROTO_COMP:
-		return "comp"
-	case unix.IPPROTO_DCCP:
-		return "dccp"
-	case unix.IPPROTO_SCTP:
-		return "sctp"
-	}
-	return "unknown"
-}
 
 func (pkt *PacketInfo) Decode(b []byte) error {
 	ad, err := netlink.NewAttributeDecoder(b[data.NlNftAttrOffset:])
